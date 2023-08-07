afbc4c41543b1dd3a3a8aa8514c583e6052871d7
sgoudham <sgoudham@gmail.com>
2023-08-07 01:25:59 +0100

    refactor: properly size images
    64px is the highest resolution
    that the image is displayed at.

--- app/components/images/ProfilePicture.tsx
+++ app/components/images/ProfilePicture.tsx
@@ -4,8 +4,8 @@ export const ProfilePicture = ({}) => {
   return (
     <Image
       src="/profile-picture.png"
-      width={400}
-      height={400}
+      width={64}
+      height={64}
       alt="Goudham smiling at the camera"
       className="motion-safe:hover:animate-spin w-12 h-12 lg:w-14 lg:h-14 xl:w-16 xl:h-16"
       quality={100}
