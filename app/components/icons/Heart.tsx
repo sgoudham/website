b3e4f3cf7331eb45cfe6b8408d37801d86df7526
sgoudham <sgoudham@gmail.com>
2023-08-07 01:36:47 +0100

    fix(icons): warnings in svgs

--- app/components/icons/Heart.tsx
+++ app/components/icons/Heart.tsx
@@ -9,9 +9,9 @@ export const Heart = () => {
         d="M128,216S24,160,24,94A54,54,0,0,1,78,40c22.59,0,41.94,12.31,50,32,8.06-19.69,27.41-32,50-32a54,54,0,0,1,54,54C232,160,128,216,128,216Z"
         fill="red"
         stroke="currentColor"
-        stroke-linecap="round"
-        stroke-linejoin="round"
-        stroke-width="0"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        strokeWidth="0"
       />
     </svg>
   );
--- app/components/icons/Wave.tsx
+++ app/components/icons/Wave.tsx
@@ -10,41 +10,41 @@ export const Wave = () => {
         d="M98,64.31a20,20,0,0,1,34.64-20l28,48.5"
         fill="none"
         stroke="currentColor"
-        stroke-linecap="round"
-        stroke-linejoin="round"
-        stroke-width="16"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        strokeWidth="16"
       />
       <path
         d="M71.35,98.17l-16-27.71A20,20,0,0,1,90,50.46l34,58.89"
         fill="none"
         stroke="currentColor"
-        stroke-linecap="round"
-        stroke-linejoin="round"
-        stroke-width="16"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        strokeWidth="16"
       />
       <path
         d="M156,164.77a40,40,0,0,1,14.64-54.64l-10-17.32a20,20,0,0,1,34.64-20l18,31.18A80,80,0,0,1,74.7,184l-38-65.82a20,20,0,0,1,34.65-20l22,38.1"
         fill="none"
         stroke="currentColor"
-        stroke-linecap="round"
-        stroke-linejoin="round"
-        stroke-width="16"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        strokeWidth="16"
       />
       <path
         d="M192,32a52.12,52.12,0,0,1,32,24"
         fill="none"
         stroke="currentColor"
-        stroke-linecap="round"
-        stroke-linejoin="round"
-        stroke-width="16"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        strokeWidth="16"
       />
       <path
         d="M74.62,232c-11-8.69-19.39-19.48-26.62-32"
         fill="none"
         stroke="currentColor"
-        stroke-linecap="round"
-        stroke-linejoin="round"
-        stroke-width="16"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        strokeWidth="16"
       />
     </svg>
   );
