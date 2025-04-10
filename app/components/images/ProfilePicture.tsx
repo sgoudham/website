d816d3b4dadf0754289a33316888bf3dc6552658
sgoudham <sgoudham@gmail.com>
2025-04-11 00:44:42 +0100

    fix: don't use nextjs image component

--- app/components/images/ProfilePicture.tsx
+++ app/components/images/ProfilePicture.tsx
@@ -1,15 +1,12 @@
-import Image from "next/image";
-
-export const ProfilePicture = ({ }) => {
+export const ProfilePicture = ({}) => {
   return (
-    <Image
+    <img
       src="/profile-picture.png"
       width={128}
       height={128}
       alt="Goudham smiling at the camera"
       className="motion-safe:hover:animate-spin w-20 h-20 lg:w-24 lg:h-24 xl:w-28 xl:h-28 2xl:w-32 2xl:h-32"
-      quality={100}
-      priority={true}
+      fetchPriority="high"
     />
   );
 };
